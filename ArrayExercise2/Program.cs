7b7bb2ee59ab19ea875d4db7ae198ddb3d45d1bb
waelhawa <wael.hawa86@gmail.com>
2021-04-21 19:57:09 -0400

    First Commit

---
+++ ArrayExercise2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArrayExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "wael", "isha", "anna", "anthony", "david", "george" };
+            string[] arrangedNames = new string[names.Length];
+            string[] arrangedLettersInNames = new string[names.Length];
+            for (int i = 0; i < names.Length; i++)
+            {
+                arrangedNames[i] = names[i];
+            }
+            Array.Sort(arrangedNames);
+            //foreach (string i in arrangedNames)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            for (int i = 0; i < arrangedNames.Length; i++)
+            {
+                char [] sorter = arrangedNames[i].ToCharArray();
+                Array.Sort(sorter);
+                arrangedLettersInNames[i] = string.Join("", sorter);
+            }
+
+            for (int i = 0; i < arrangedNames.Length; i++)
+            {
+                Console.WriteLine($"{names[i]}\t\t{arrangedNames[i]}\t\t{arrangedLettersInNames[i]}");
+            }
+        }
+    }
+}
